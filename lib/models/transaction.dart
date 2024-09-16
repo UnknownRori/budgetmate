34c493ea9b8f051c594a6ec445f085c3e06584a9
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-09-17 01:46:02 +0700

    📦 add transaction class and it's repository

---
+++ lib/models/transaction.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+enum TransactionType {
+  income,
+  expense,
+}
+
+Map<String, IconData> categoryIcons = {
+  'Salary': Icons.attach_money,
+  'Shopping': Icons.shopping_cart,
+  'Electricity': Icons.electrical_services,
+  'Transport': Icons.directions_bus,
+  'Entertainment': Icons.movie,
+  'Rent': Icons.home,
+};
+
+class Transaction {
+  String id;
+  TransactionType type;
+  double amount;
+  DateTime date;
+  String category;
+
+  Transaction({
+    String? id,
+    required this.type,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = id ?? const Uuid().v4();
+
+  // Get the icon for the category
+  // Default to 'help' if no icon is found
+
+  IconData getCategoryIcon() {
+    return categoryIcons[category] ?? Icons.help;
+  }
+}
+
+class TransactionRepository {
+  // Default dummy data
+  List<Transaction> data = [
+    Transaction(
+        type: TransactionType.income,
+        date: DateTime.now(),
+        amount: 3000000,
+        category: "Salary"),
+    Transaction(
+        type: TransactionType.expense,
+        date: DateTime.now(),
+        amount: 250000,
+        category: "Shopping"),
+    Transaction(
+        type: TransactionType.expense,
+        date: DateTime.now(),
+        amount: 100000,
+        category: "Electricity"),
+    Transaction(
+        type: TransactionType.expense,
+        date: DateTime.now(),
+        amount: 150000,
+        category: "Rent"),
+    Transaction(
+        type: TransactionType.expense,
+        date: DateTime.now(),
+        amount: 100000,
+        category: "Transport"),
+    Transaction(
+        type: TransactionType.expense,
+        date: DateTime.now(),
+        amount: 150000,
+        category: "Entertainment"),
+  ];
+
+  void addTransaction(Transaction transaction) {
+    data.add(transaction);
+  }
+
+  List<Transaction> getAll() {
+    return List.unmodifiable(data);
+  }
+
+  bool removeTransactionById(String id) {
+    final transactionIndex =
+        data.indexWhere((transaction) => transaction.id == id);
+
+    if (transactionIndex != -1) {
+      data.removeAt(transactionIndex);
+      return true;
+    }
+
+    return false;
+  }
+}
