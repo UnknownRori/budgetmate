2c0d5e19a0c404c84801e8f60e5f8fa30cce4263
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-09-16 10:59:56 +0700

    📦 implement navigation

---
+++ lib/models/budget_type.dart
@@ -0,0 +1,4 @@
+enum BudgetType {
+  income,
+  expense,
+}
---
+++ lib/page/add_budget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddBudgetPage extends StatelessWidget {
+  const AddBudgetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Hi AddBudget!"));
+  }
+}
